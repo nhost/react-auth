01659c8091dc422740a69c7f87124b10bf3279c2
Cezar Neaga <hi@cezarneaga.eu>
2020-10-22 15:11:14 +0200

    remove commented code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React, { createContext, useState, useContext, useEffect } from 'react'
 export const AuthContext = createContext({ signedIn: null })
 
-// export function NhostAuthProvider({ auth, children }) {
-//   const [signedIn, setSignedIn] = useState(auth.isAuthenticated())
-
-//   useEffect(() => {
-//     return auth.onAuthStateChanged((data) => {
-//       setSignedIn(data)
-//     })
-//   }, [auth.onAuthStateChanged])
-
-//   return (
-//     <AuthContext.Provider value={{ signedIn }}>{children}</AuthContext.Provider>
-//   )
-// }
 export function NhostAuthProvider({ auth, children }) {
   const [signedIn, setSignedIn] = useState()
   useEffect(() => {
